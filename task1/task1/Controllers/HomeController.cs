2e2a7b38baf570135eb014fc45b8d0ddc3e71d93
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-04-03 17:06:26 +0300

    Add files via upload
    proje sonu

---
+++ task1/task1/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using FastMember;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using models.BaseClass;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+
+namespace task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //Bu kısımda controller üzerinde belirtilen url'e günümüz tarihini parametre olarak yollayarak get request oluşturup,
+        //Gelen veri üzerinde linq kullanarak değişiklikler yapıyoruz.Son olarak oluşturduğumuz listeyi view'e dönüyoruz.
+        public async Task<IActionResult> Index()
+        {
+            var responseObj = new Result();
+            using (var client = new HttpClient())
+            {
+                var dts = DateTime.Now;
+                var requestParams = $"?endDate={dts.Year}-{dts.Month}-{dts.Day}&startDate={dts.Year}-{dts.Month}-{dts.Day}";
+                var baseUrl = "https://seffaflik.epias.com.tr/transparency/service/market/intra-day-trade-history";
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = new HttpResponseMessage();
+
+                response = await client.GetAsync(baseUrl + requestParams).ConfigureAwait(false);
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    responseObj = JsonConvert.DeserializeObject<Result>(result);
+                }
+            }
+            var list = responseObj.body.intraDayTradeHistoryList;
+            //Başında pb olmayan conractlar
+            var conracts = list.Where(x => !x.Conract.StartsWith("PB")).Select(x => x.Conract).Distinct().ToList();
+            var table = new List<TableObject>();
+            foreach (var conract in conracts)
+            {
+                //bu conractların aynı isimli olanları
+                var currentConractList = list.Where(x => x.Conract == conract).ToList();
+
+                var tableObject = new TableObject();
+                //tablo içinde dönüyoruz, istenilen kısımları hesaplıyoruz
+                foreach (var currentConract in currentConractList)
+                {
+                    tableObject.TotalProcessPrice += (currentConract.Price * currentConract.Quantity) / 10;
+                    tableObject.TotalProcessCount += (currentConract.Quantity) / 10;
+                }
+                //son olarak oluşan datalarla ortalamayı hesaplıyoruz
+                tableObject.AvaragePrice = tableObject.TotalProcessPrice / tableObject.TotalProcessCount;
+                //conractın ismini substringlere bölerek ilgili formattan parse ediyoruz
+                tableObject.DateTime = new DateTime(year: Int32.Parse(conract.Substring(2, 2)), month: Int32.Parse(conract.Substring(4, 2)), day: Int32.Parse(conract.Substring(6, 2)), hour: Int32.Parse(conract.Substring(8, 2)), minute: 0, second: 0);
+                //oluşan objeleri bir tabloda topluyoruz
+                table.Add(tableObject);
+            }
+            //bu tabloyu view a dönüyoruz
+            return View(table);
+        }
+    }
+}
---
+++ task1/task1/Models/Classes.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+namespace models.BaseClass
+{
+
+    public class Result
+    {
+        public int resultCode { get; set; }
+        public string resultDescription { get; set; }
+
+        public Body body { get; set; }
+
+
+    }
+    public class IntraDayTradeHistory
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Conract { get; set; }
+
+        public double Price { get; set; }
+
+        public double Quantity { get; set; }
+    }
+    public class Body
+    {
+        public List<IntraDayTradeHistory> intraDayTradeHistoryList { get; set; }
+    }
+    public class ResultTableObject
+    {
+        public List<TableObject> Table { get; set; }
+    }
+    public class TableObject
+    {
+        public DateTime DateTime { get; set; }
+        public double TotalProcessCount { get; set; }
+        public double TotalProcessPrice { get; set; }
+        public double AvaragePrice { get; set; }
+
+    }
+}
